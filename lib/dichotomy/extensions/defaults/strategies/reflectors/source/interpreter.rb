38be4d8717f60d05b81c381b6c7b318bcb592497
Rino André Johnsen <rinoandrejohnsen@gmail.com>
2014-07-11 03:04:35 +0200

    Fixed a bug in the Interpreter class

--- lib/dichotomy/extensions/defaults/strategies/reflectors/source/interpreter.rb
+++ lib/dichotomy/extensions/defaults/strategies/reflectors/source/interpreter.rb
@@ -20,7 +20,6 @@ module Dichotomy
                 @rapport.name = name[/([^\/]+)$/].chomp('.rb')
 
                 find_methods
-                #find_dependencies
               end
 
               def create_rapport(buffer, ast)
@@ -52,59 +51,27 @@ module Dichotomy
                 end
               end
 
-
-
-
-
-              #       arguments_node = child_node.children[1]
-              #
-              #
-              #
-              #       arguments_node.children.each do |args_child_node|
-              #         args_child_node.children.each do |arg|
-              #           if child_node.children[0].eql? :initialize # add check for Symbol
-              #
-              #             methods_on_variable = Array.new
-              #             if child_node.children[2]
-              #               child_node.children[2].children.each do |variable|
-              #                 if variable.children[0].kind_of?(Symbol)
-              #
-              #                   @rapport.dependencies[arg] = look_for_methods_on_variable(variable.children[0])
-              #
-              #                   rino = 22
-              #                 end
-              #               end
-              #             end
-              #           end
-              #           argumentsHash.push(arg)
-              #         end
-              #       end
-              #
-              #       if !child_node.children[0].eql? :initialize
-              #
-              #       end
-              #     end
-              #   end
-              # end
-
               def find_dependencies(node)
                 node.children[1].children.each_with_index do |dependency_variable, index|
                   if node.children[2]
-                      variable = node.children[2].children[index]
+                    variable = node.children[2].children[index]
+
+                    if (variable.kind_of?(Symbol))
+                      @rapport.dependencies[dependency_variable.children[0]] = look_for_methods_on_variable(variable)
+                    else
                       if variable.children[0].kind_of?(Symbol)
-
                         @rapport.dependencies[dependency_variable.children[0]] = look_for_methods_on_variable(variable.children[0])
-
-                        rino = 22
                       end
+                    end
                   end
                 end
               end
 
+
               def look_for_methods_on_variable(variables)
                 methods = Array.new
                 @node.children.each do |child_node|
-                  if child_node.location.kind_of?(Parser::Source::Map::Definition)
+                  if child_node.location.kind_of?(Parser::Source::Map::Definition) && !child_node.children[0].eql?(:initialize)
                     argumentsHash = Array.new
 
                     child_node.children[2].children.each do |variable|
--- lib/example_dependency_two.rb
+++ lib/example_dependency_two.rb
@@ -1,12 +1,15 @@
 class ExampleDependencyTwo
-  def initialize(dependency, rino)
+  def initialize(dependency, rino, andre)
     @dependency = dependency
     @rino = rino
+    @andre = andre
   end
 
   def init(tt)
     @dependency.run
     @dependency.pause
     @dependency.stop
+
+    @andre.johnsen
   end
 end
